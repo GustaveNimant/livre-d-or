8aae119e8a9d82e7467330345a958fca2f8ba1fa
GustaveNimant <achadde@iabloko.fecamp>
2019-07-01 10:36:13 +0200

    00:24:40 request.session.error ='Il y a une erreur'

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ let express = require ('express');
 let app = express ();
 
 let bodyParser = require ('body-parser');
+let session = require ('express-session');
 
 // Middleware body-parser
 app.use(bodyParser.urlencoded({extended: false}));
@@ -15,14 +16,24 @@ app.set ('view engine', 'ejs');
 // fichiers statics /assets/public
 app.use('/assets', express.static('public'));
 
+// sessions cookie
+app.use(session({
+  secret: 'blablabla',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } /* pas de https */
+}));
+
 // Routing : 
 app.get ('/', (request, response) => {
+    console.log(request.session.error);
     response.render ('pages/index', {test: 'Ceci est un test'})
 });
 
 app.post ('/', (request, response) => {
     if (request.body.message === undefined ||request.body.message === '' ){
-	response.render('pages/index', {error :"Vous n'avez pas posté de message"});
+	request.session.error = "Il y a une erreur";
+	response.redirect ('/');
     }
 });
 
