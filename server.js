43cdbf45fdf9d499c3954aa218cea0785db6fe55
GustaveNimant <achadde@iabloko.fecamp>
2019-07-01 08:09:35 +0200

    00:11:50 CSS as /assets

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ let port = 8080;
 // Moteur de template
 app.set ('view engine', 'ejs');
 
+// fichiers statics /assets/public
+app.use('/assets', express.static('public'));
+
 // Routing : 
 app.get ('/', (request, response) => {
     response.render ('pages/index', {test: 'Ceci est un test'})
